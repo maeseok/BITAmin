7bc2fdcdcc776dfed9949aea1fdc536088a5baf0
maeseok <yunbum37@daum.net>
2024-02-13 21:27:00 +0900

    0213

--- 겨울방학 프로젝트/Rcode_4share.R
+++ 겨울방학 프로젝트/Rcode_4share.R
@@ -1,19 +1,20 @@
 
 install.packages("readxl")
-library(readxl)
 install.packages("lmtest")
-library(lmtest)
 install.packages("urca")
-library(urca)
 install.packages("forecast")
-library(forecast)
 install.packages("vars")
-library(vars)
 install.packages("tseries")
+
+library(readxl)
+library(lmtest)
+library(urca)
+library(forecast)
+library(vars)
 library(tseries)
 
 #월별수익률 ts객체 설정
-setwd("C:/Users/LWG/Desktop/2023/코딩/BITAmin/겨울방학 프로젝트")
+setwd("C:/Users/gudtj/OneDrive/바탕 화면/2023/코딩/BITAmin/겨울방학 프로젝트")
 return <- read_xlsx('KCC_monthlyReturn.xlsx', sheet=1)
 return <- ts(return[[2]], start = c(2013,1), frequency = 12)
 return
@@ -24,10 +25,10 @@ return
 
 data <- read_xlsx('data.xlsx', sheet=1)
 
-adf.test(data[[2]])
+adf.test(data[[2]]) #시계열의 정상성 검정
 pp.test(data[[2]])
 
-adf.test(diff(data[[2]]))
+adf.test(diff(data[[2]])) #정상성 만족하지 않아 1차 차분
 pp.test(diff(data[[2]]))
 
 var1 <- ts(diff(data[[2]])[3:length(diff(data[[2]]))], start = c(2013,1), frequency = 12)
@@ -86,7 +87,7 @@ adf.test(diff(data[[7]]))
 pp.test(diff(data[[7]]))
 
 adf.test(diff(diff(data[[7]])))
-adf.test(diff(diff(data[[7]])))
+pp.test(diff(diff((data[[7]]))))
 
 var6 <- ts(diff(diff(data[[7]]))[2:length(diff(diff(data[[7]])))], start = c(2013,1), frequency = 12)
 var6
@@ -100,7 +101,7 @@ adf.test(diff(data[[8]]))
 pp.test(diff(data[[8]]))
 
 adf.test(diff(diff(data[[8]])))
-adf.test(diff(diff(data[[8]])))
+pp.test(diff(diff((data[[8]]))))
 
 var7 <- ts(diff(diff(data[[8]]))[2:length(diff(diff(data[[8]])))], start = c(2013,1), frequency = 12)
 var7
@@ -203,7 +204,7 @@ grangertest(return~var1, order=1) # var1 인과관계x
 
 v2 <- cbind(return, var2)
 VARselect(v2, lag.max=12, type='const') # BIC = 2
-grangertest(return~var2, order=2) # var2 인과관계x
+grangertest(return~var2, order=1) # var2 인과관계x
 
 v3 <- cbind(return, var3)
 VARselect(v3, lag.max=12, type='const') # BIC = 1
@@ -226,15 +227,15 @@ VARselect(v7, lag.max=12, type='const') # BIC = 3
 grangertest(return~var7, order=3) # var7 인과관계x
 
 v8 <- cbind(return, var8)
-VARselect(v8, lag.max=12, type='const') # BIC = 1
-grangertest(return~var8, order=1) # var8 인과관계o -> 0.02951
+VARselect(v8, lag.max=12, type='const') # BIC = 2
+grangertest(return~var8, order=2) # var8 인과관계o -> 0.02951
 
 v9 <- cbind(return, var9)
 VARselect(v9, lag.max=12, type='const') # BIC = 1
 grangertest(return~var9, order=1) # 
 
 v10 <- cbind(return, var10)
-VARselect(v10, lag.max=12, type='const') # BIC =
+VARselect(v10, lag.max=12, type='const') # BIC = 1
 grangertest(return~var10, order=1) # var10 인과관계o -> 0.01197
 
 v11 <- cbind(return, var11)
@@ -246,49 +247,88 @@ VARselect(v12, lag.max=12, type='const') # BIC = 1
 grangertest(return~var12, order=1) # 
 
 v13 <- cbind(return, var13)
-VARselect(v13, lag.max=12, type='const') # BIC = 1
-grangertest(return~var13, order=1) # var13 인과관계o -> 0.01925
+VARselect(v13, lag.max=12, type='const') # BIC = 2
+grangertest(return~var13, order=2) # var13 인과관계o -> 0.01925
 
 v14 <- cbind(return, var14)
-VARselect(v14, lag.max=12, type='const') # BIC = 1
-grangertest(return~var14, order=1) # var14 인과관계o -> 0.008534
+VARselect(v14, lag.max=12, type='const') # BIC = 12
+grangertest(return~var14, order=12) # var14 인과관계x
 
 v15 <- cbind(return, var15)
 VARselect(v15, lag.max=12, type='const') # BIC = 1
 grangertest(return~var15, order=1) # 
 
 
-#인과관계가 있는 변수 : var8(경제심리지수), var10(규소가격), var13(건설수주액_경상), var14(건설기성액_경상)
+#인과관계가 있는 변수 : var8(경제심리지수), var10(규소가격), var13(건설수주액_경상)
 
-#VAR 모델링 -> 다시해보기
+#VAR 모델링 
 new_return <- window(return, start = c(2021,1))
-plot(new_return, xlab="", ylab="", lwd=2)
+lower_limit <- -160
+upper_limit <- 120
+plot(new_return, xlab="", ylab="", lwd=2, ylim=c(lower_limit, upper_limit))
 grid()
 
-
-v_final <- cbind(return, var8, var10, var13, var14)
+v_final <- cbind(return,var8, var10,var13)
 v_final_1 <- window(v_final, end = c(2022,6))
 
-VARselect(v_final_1, lag.max = 12, type='const')  # BIC = 1
+VARselect(v_final_1, lag.max = 12, type='const')
 
-VAR_model_1 <- VAR(v_final_1, p =1, type='const')
+#BIC
+VAR_model_1 <- VAR(v_final_1, p =2, type='const')
 forecast <- predict(VAR_model_1, n.ahead=6)
-
 var1_fcst <- ts(forecast$fcst$return[,1], start = c(2022,7), frequency = 12)
 lines(var1_fcst, lty=2, col='blue', lwd=3)
-legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=1)'),lty = c(1, 2),col = c('black', 'blue'))
+legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=2)'),lty = c(1, 2),col = c('black', 'blue'))
 
-
-VAR_model_2 <- VAR(v_final_1, p =11, type='const')
+#AIC
+VAR_model_2 <- VAR(v_final_1, p =12, type='const')
 forecast_2 <- predict(VAR_model_2, n.ahead=6)
 var2_fcst <- ts(forecast_2$fcst$return[,1], start = c(2022,7), frequency = 12)
 lines(var2_fcst, lty=2, col='red', lwd=3)
-legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=1)', 'VAR_Forecasts(lag=11)'),lty = c(1, 2, 2),col = c('black', 'blue', 'red'))
+legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=2)', 'VAR_Forecasts(lag=12)'),lty = c(1, 2, 2),col = c('black', 'blue', 'red'))
 
 
-
+#HQ 
 VAR_model_3 <- VAR(v_final_1, p = 2, type='const')
 forecast_3 <- predict(VAR_model_3, n.ahead=6)
 var3_fcst <- ts(forecast_3$fcst$return[,1], start = c(2022,7), frequency = 12)
 lines(var3_fcst, lty=2, col='green', lwd=3)
-legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=1)', 'VAR_Forecasts(lag=11)', 'VAR_Forecasts(lag=2)'),lty = c(1, 2, 2, 2),col = c('black', 'blue', 'red', 'green'))
+legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=1)', 'VAR_Forecasts(lag=12)', 'VAR_Forecasts(lag=2)'),lty = c(1, 2, 2, 2),col = c('black', 'blue', 'red', 'green'))
+
+#rtn <- window(return, end=c(2022,6))
+#pacf(rtn, lag=12, main="")
+
+
+#AR
+AR_model <- arima(rtn, order = c(9,0,0), method="CSS")
+AR_fcst <- predict(AR_model, 6)$pred
+lines(AR_fcst, lty=2, col='purple', lwd=3)
+legend('bottomleft', legend = c('Observations', 'VAR_Forecasts(lag=1)', 'VAR_Forecasts(lag=11)', 'VAR_Forecasts(lag=2)', 'AR_Forecasts'),lty = c(1, 2, 2, 2, 2),col = c('black', 'blue', 'red', 'green', 'purple'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
